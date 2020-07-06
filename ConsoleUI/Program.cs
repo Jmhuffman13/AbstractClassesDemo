4246fd7b6db50776c5da8d8354add31b7be7531b
John Huffman <jmhuffman37@gmail.com>
2020-07-06 12:25:37 -0500

    finished exercise

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -17,34 +17,53 @@ namespace ConsoleUI
             #region Vehicles
 
             /*
-             * Create an abstract class called Vehicle
-             * The vehicle class shall have three string properties Year, Make, and Model
-             * Set the defaults to something generic in the Vehicle class
-             * Vehicle shall have an abstract method called DriveAbstract with no implementation
-             * Vehicle shall have a virtual method called DriveVirtual with a base implementation.
+             * Create an abstract class called Vehicle - DONE
+             * The vehicle class shall have three string properties Year, Make, and Model - DONE 
+             * Set the defaults to something generic in the Vehicle class - DONE
+             * Vehicle shall have an abstract method called DriveAbstract with no implementation - DONE
+             * Vehicle shall have a virtual method called DriveVirtual with a base implementation. - DONE
              */
 
             /* 
-             * Now create 2 non-abstract classes: Car and Motorcycle, that inherit from Vehicle
-             * Add a distict property in the 2 derived classes such as HasTrunk for Car and HasSideCart for Motorcycle
-             * Provide the implementations for the abstract methods
-             * Only in the Motorcycle class will you override the virtual drive method
+             * Now create 2 non-abstract classes: Car and Motorcycle, that inherit from Vehicle - DONE
+             * Add a distict property in the 2 derived classes such as HasTrunk for Car and HasSideCart for Motorcycle - DONE
+             * Provide the implementations for the abstract methods -DONE
+             * Only in the Motorcycle class will you override the virtual drive method - DONE
             */
 
-            // Create a list of Vehicle called vehicles
+            // Create a list of Vehicle called Vehicles - DONE
+
+            var vehicles = new List<Vehicle>();
 
             /*
              * Create 4 instances: 1 Car, 1 Motorcycle, and then 2 instances of type Vehicle (use explicit typing) but use constuctors from derived classes
              * - new it up as one of each derived class
-             * Set the properties with object initializer syntax
+             * Set the properties with object initializer syntax -DONE
              */
+
+            Car explorer = new Car() { HasTrunk = true, Make = "Ford", Model = "Explorer", Year = 2019 };
+            Motorcycle harley = new Motorcycle() { HasTwoWheels = true, Make = "Harley Davidson", Model = "fatback", Year = 2017 };
+            Vehicle sedan = new Car() { HasTrunk = true, Make = "Honda", Model = "Accord", Year = 2020 };
+            Vehicle sportsCar = new Car() { HasTrunk = false, Make = "Rolls Royce", Model = "Phantom", Year = 2015 };
+
 
             /*
              * Add the 4 vehicles to the list
-             * Using a foreach loop iterate over each of the properties
+             * Using a foreach loop iterate over each of the properties - DONE
              */
+            vehicles.Add(explorer);
+            vehicles.Add(harley);
+            vehicles.Add(sedan);
+            vehicles.Add(sportsCar);
 
-            // Call each of the drive methods for one car and one motorcycle
+            foreach (var veh in vehicles)
+            {
+                Console.WriteLine($"Make: {veh.Make} Model: {veh.Model} Year: {veh.Year}");
+                veh.DriveAbstract();
+                Console.WriteLine("--------------------------");
+            }
+
+            // Call each of the drive methods for one car and one motorcycle 
 
             #endregion            
             Console.ReadLine();
---
+++ ConsoleUI/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleUI
+{
+    public abstract class Vehicle
+    {
+        public int Year { get; set; } = 2019;
+        public string Make { get; set; } = "Default Make";
+        public string Model { get; set; } = "Defaut Model";
+
+        public abstract void DriveAbstract(); // stubbed out method - has no implementation
+        public virtual void DriveVirtual()
+        {
+            Console.WriteLine($"This {GetType().Name} is virtually in drive."); // implementation
+        }
+
+    }
+}
